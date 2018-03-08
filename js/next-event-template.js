7c8dc4e3efafb63198a2a71ba858a3143c6b93e7
minaorangina <wilhelmina.jane@gmail.com>
2018-03-08 18:32:07 +0000

    Adds last event to past events
    The PR looks really big because Prettier got involved ;)

--- js/next-event-template.js
+++ js/next-event-template.js
@@ -1,16 +1,18 @@
 fetch(DATA_URL)
-  .then(function (res) {
+  .then(function(res) {
     return res.json();
   })
   .then(sort)
   .then(handleData)
-  .then(function (html) {
-    document.querySelector('.future-events-container').innerHTML = html.futureHTML;
+  .then(function(html) {
+    document.querySelector(".future-events-container").innerHTML =
+      html.futureHTML;
   })
   .catch(console.err);
 
-function sort (data) {
-  var futureEvents = data.filter(function (event) {
+function sort(data) {
+  debugger;
+  var futureEvents = data.filter(function(event) {
     return event.date >= TODAY;
   });
   return {
@@ -18,7 +20,7 @@ function sort (data) {
   };
 }
 
-function handleData (events) {
+function handleData(events) {
   var futureHTML;
   if (events.futureEvents.length === 0) {
     futureHTML =
@@ -28,7 +30,7 @@ function handleData (events) {
       for updates!\
       </p>';
   } else {
-    futureHTML = events.futureEvents.reduce(generateHTML, '');
+    futureHTML = events.futureEvents.reduce(generateHTML, "");
   }
   return {
     futureHTML: futureHTML
@@ -37,47 +39,60 @@ function handleData (events) {
 
 /***************/
 
-function generateHTML (finalHTML, event) {
+function generateHTML(finalHTML, event) {
+  debugger;
   var currentEventHTML = template;
 
   for (var key in event) {
-    var value = '';
+    var value = "";
     switch (key) {
-    case 'date':
-      value = moment(event.date).format('dddd Do MMMM YYYY');
-      break;
-    case 'application_text':
-      value = generateApplicationText(event);
-      break;
-    case 'sponsors':
-      value = generateSponsors(event.sponsors);
-      break;
-    default:
-      value = event[key] || 'TBC'
+      case "date":
+        value = moment(event.date).format("dddd Do MMMM YYYY");
+        break;
+      case "application_text":
+        value = generateApplicationText(event);
+        break;
+      case "sponsors":
+        value = generateSponsors(event.sponsors);
+        break;
+      default:
+        value = event[key] || "TBC";
     }
-    currentEventHTML = currentEventHTML.replace(new RegExp('{{' + key + '}}', 'g'), value);
+    currentEventHTML = currentEventHTML.replace(
+      new RegExp("{{" + key + "}}", "g"),
+      value
+    );
   }
   return finalHTML + currentEventHTML;
 }
 
-function generateApplicationText (event) {
+function generateApplicationText(event) {
   // if the event is in the future, include application text
   // add a link if there is one
-  var text = '';
-  if (moment(event.date).isAfter(TODAY))  {
-    text = '<span class="application-text">' + event.application_text + '</span>';
+  var text = "";
+  if (moment(event.date).isAfter(TODAY)) {
+    text =
+      '<span class="application-text">' + event.application_text + "</span>";
     if (event.application_link && event.application_link.length > 0) {
-      text = '<a href="' + event.application_link + '">' + text + '</a>';
+      text = '<a href="' + event.application_link + '">' + text + "</a>";
     }
   }
   return text;
 }
 
-function generateSponsors (sponsors) {
+function generateSponsors(sponsors) {
   return sponsors.reduce((html, sponsor) => {
-    html = html + '<a target="_blank" href="' + sponsor.link + '">\
-      <img class="sponsor" src="' + sponsor.logo + '" alt="' + sponsor.name + '">\
-    </a>'
+    html =
+      html +
+      '<a target="_blank" href="' +
+      sponsor.link +
+      '">\
+      <img class="sponsor" src="' +
+      sponsor.logo +
+      '" alt="' +
+      sponsor.name +
+      '">\
+    </a>';
     return html;
-  }, '');
+  }, "");
 }
