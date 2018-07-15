6bdbe8aa8a0fb15165fc18de8f4d6924585695d6
Mina <minaorangina@users.noreply.github.com>
2018-07-15 11:55:53 +0100

    Update variables.js

--- js/variables.js
+++ js/variables.js
@@ -5,7 +5,7 @@ var destination = document.querySelector('.events');
 // PRODUCTION URL
 // this requires the latest commit hash in the events-data repo (the merge commit from the last PR)
 // Can be the short or long version.
-var commitHash = "e70f603";
+var commitHash = "a8538a6";
 var prodUrl = "https://cdn.rawgit.com/node-girls/events-data/" + commitHash + "/events.json";
 
 // DEVELOPMENT URL
