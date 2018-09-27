16949b8b44b8c8b31a216f7aaab6751e077c20ec
minaorangina <wilhelmina.jane@gmail.com>
2018-09-27 10:57:26 +0100

    update data commit hash

--- js/variables.js
+++ js/variables.js
@@ -1,11 +1,10 @@
-var template = document.querySelector('#event-template').innerHTML;
-var destination = document.querySelector('.events');
-
+var template = document.querySelector("#event-template").innerHTML;
+var destination = document.querySelector(".events");
 
 // PRODUCTION URL
 // this requires the latest commit hash in the events-data repo (the merge commit from the last PR)
 // Can be the short or long version.
-var commitHash = "f8b482a";
+var commitHash = "76879546";
 var prodUrl = "https://cdn.rawgit.com/node-girls/events-data/" + commitHash + "/events.json";
 
 // DEVELOPMENT URL
@@ -16,8 +15,8 @@ var devUrl = "https://rawgit.com/node-girls/events-data/" + yourWorkingBranchNam
 
 // LOCAL URL
 // Use if you are serving your events-data repo from a local server (e.g. simple-http-server)
-var localUrl = ""
+var localUrl = "";
 
 var DATA_URL = prodUrl; // change to devUrl or localURL as necessary
 
-var TODAY = moment().format('YYYY-MM-DD');
+var TODAY = moment().format("YYYY-MM-DD");
