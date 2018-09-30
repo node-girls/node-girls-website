23ed624105623626daec640099db0bc98d48a64d
Mina <wilhelmina.jane@gmail.com>
2018-09-30 16:03:40 +0100

    adds first event

--- js/variables.js
+++ js/variables.js
@@ -4,19 +4,26 @@ var destination = document.querySelector(".events");
 // PRODUCTION URL
 // this requires the latest commit hash in the events-data repo (the merge commit from the last PR)
 // Can be the short or long version.
-var commitHash = "76879546";
-var prodUrl = "https://cdn.rawgit.com/node-girls/events-data/" + commitHash + "/events.json";
+var commitHash = "5a834c86";
+var prodUrl =
+  "https://cdn.rawgit.com/node-girls/events-data/" +
+  commitHash +
+  "/events.json";
 
 // DEVELOPMENT URL
 // To see if your updated events.json is working, set this variable to the name
 // of your working branch on the events-data repo (after you've pushed to GitHub)
 var yourWorkingBranchName = "";
-var devUrl = "https://rawgit.com/node-girls/events-data/" + yourWorkingBranchName + "/events.json";
+var devUrl =
+  "https://rawgit.com/node-girls/events-data/" +
+  yourWorkingBranchName +
+  "/events.json";
 
 // LOCAL URL
 // Use if you are serving your events-data repo from a local server (e.g. simple-http-server)
+
 var localUrl = "";
 
-var DATA_URL = prodUrl; // change to devUrl or localURL as necessary
+var DATA_URL = prodUrl; // change to devUrl or localUrl as necessary
 
 var TODAY = moment().format("YYYY-MM-DD");
