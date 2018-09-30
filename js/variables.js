c022ef7c952d2325d17db9d3ecfab9305e9ef4ed
Mina <wilhelmina.jane@gmail.com>
2018-09-30 21:06:51 +0100

    updates data commit hash

--- js/variables.js
+++ js/variables.js
@@ -4,7 +4,7 @@ var destination = document.querySelector(".events");
 // PRODUCTION URL
 // this requires the latest commit hash in the events-data repo (the merge commit from the last PR)
 // Can be the short or long version.
-var commitHash = "5a834c86";
+var commitHash = "b4c725878";
 var prodUrl =
   "https://cdn.rawgit.com/node-girls/events-data/" +
   commitHash +
