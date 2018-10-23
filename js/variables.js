ce19e7e843a1efb36a4f73b50603ecf7763a6241
minaorangina <wilhelmina.jane@gmail.com>
2018-10-23 16:56:26 +0100

    adds london event

--- js/variables.js
+++ js/variables.js
@@ -4,7 +4,7 @@ var destination = document.querySelector(".events");
 // PRODUCTION URL
 // this requires the latest commit hash in the events-data repo (the merge commit from the last PR)
 // Can be the short or long version.
-var commitHash = "06eb2a0d7";
+var commitHash = "10dd9ac24";
 var prodUrl =
   "https://cdn.rawgit.com/node-girls/events-data/" +
   commitHash +
