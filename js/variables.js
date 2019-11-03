1c33ea8a598ee42ba60910c8772d4c1131d9a4c5
Mina <wilhelmina.jane@gmail.com>
2019-11-03 13:40:19 +0000

    adds next madrid event

--- js/variables.js
+++ js/variables.js
@@ -4,7 +4,7 @@ var destination = document.querySelector('.events');
 // PRODUCTION URL
 // this requires the latest commit hash in the events-data repo (the merge commit from the last PR)
 // Can be the short or long version.
-var commitHash = 'd03681804';
+var commitHash = '5b7c9ed7';
 var prodUrl =
   'https://cdn.rawgit.com/node-girls/events-data/' +
   commitHash +
